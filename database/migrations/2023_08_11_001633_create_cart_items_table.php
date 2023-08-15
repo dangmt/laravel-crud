066fe18b5a0f48c562003da4cc8a9c2ae57083c1
toan <dangtoanminh875@gmail.com>
2023-08-16 06:23:58 +0700

    finished

--- database/migrations/2023_08_11_001633_create_cart_items_table.php
+++ database/migrations/2023_08_11_001633_create_cart_items_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('product_id');
             $table->integer('quantity');
             $table->timestamps();
 
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
         });
     }
 
